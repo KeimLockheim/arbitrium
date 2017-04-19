d4c9ffd09bf1c499b4d83f0ec773e57e214cdc22
Maic Anthoine <maicanthoine@macbook-pro-de-maic.home>
2017-04-19 12:34:27 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name arbitriumApp
+ * @description
+ * # arbitriumApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('arbitriumApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arbitriumApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the arbitriumApp
+ */
+angular.module('arbitriumApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
