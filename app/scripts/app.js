2d001934151733e8dd91f0081ad9ea0305f73891
Walid Chatelain <walid.chatelain@heig-vd.ch>
2017-04-26 14:47:02 +0200

    Templates QuizzMultimedia + css v1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,6 +35,44 @@ angular
         templateUrl: 'views/quizz.html',
         controller: 'QuizzCtrl',
         controllerAs: 'quizz'
+      }).when('/quizzMultimedia', {
+        templateUrl: 'views/quizzMultimedia.html',
+        controller: 'QuizzCtrl',
+        controllerAs: 'quizz'
+      }).when('/multimediaQ1', {
+        templateUrl: 'views/multimediaQ1.html',
+        controller: 'QuizzCtrl',
+        controllerAs: 'quizz'
+      })
+      .when('/multimediaQ2', {
+        templateUrl: 'views/multimediaQ2.html',
+        controller: 'QuizzCtrl',
+        controllerAs: 'quizz'
+      })
+      .when('/multimediaQ3', {
+        templateUrl: 'views/multimediaQ3.html',
+        controller: 'QuizzCtrl',
+        controllerAs: 'quizz'
+      })
+      .when('/multimediaQ4', {
+        templateUrl: 'views/multimediaQ4.html',
+        controller: 'QuizzCtrl',
+        controllerAs: 'quizz'
+      })
+      .when('/multimediaQ5', {
+        templateUrl: 'views/multimediaQ5.html',
+        controller: 'QuizzCtrl',
+        controllerAs: 'quizz'
+      })
+      .when('/multimediaQ6', {
+        templateUrl: 'views/multimediaQ6.html',
+        controller: 'QuizzCtrl',
+        controllerAs: 'quizz'
+      })
+      .when('/multimediaQ7', {
+        templateUrl: 'views/multimediaQ7.html',
+        controller: 'QuizzCtrl',
+        controllerAs: 'quizz'
       }).otherwise({
         redirectTo: '/'
       });
