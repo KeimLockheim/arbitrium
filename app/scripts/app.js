9dae6d5193812dfc1b5341aa194c5d22b0a9dff5
Vincent Pretre <vincent.pretre@heig-vd.ch>
2017-05-01 08:30:40 +0200

    app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,7 +18,6 @@ angular.module('arbitriumApp', [
     'ngTouch',
     'dragularModule',
     'gajus.swing',
-    'angular-storage',
     'angular-storage'
   ])
   .config(function ($routeProvider) {
@@ -84,7 +83,7 @@ angular.module('arbitriumApp').run(function(AuthService, $rootScope, $route, $lo
 
 
       event.preventDefault();
-      $location.url('/login');
+      $location.url('/');
     }
   });
 });
