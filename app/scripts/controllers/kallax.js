cd8e5adff6f69588401e7435df04d0a6ee85c20b
Adrien Savoretti <adrien.savo@gmail.com>
2017-05-02 14:41:06 +0200

    Add hexagon filling display on kallax's view

--- app/scripts/controllers/kallax.js
+++ app/scripts/controllers/kallax.js
@@ -1,17 +1,61 @@
-'use strict';
+angular.module('arbitriumApp').controller('KallaxCtrl', function($scope, AuthService, $http) {
 
-/**
- * @ngdoc function
- * @name arbitriumApp.controller:TrainingCtrl
- * @description
- * # TrainingCtrl
- * Controller of the arbitriumApp
- */
-angular.module('arbitriumApp')
-  .controller('KallaxCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  // Récupère l'utilisateur actuel
+  var actualUserId = AuthService.userInf.id;
+
+  // Instantiation des variables
+  $scope.imageHexagon = 'jauge.png';
+  $scope.hrefHexagonImg = '';
+  var nbMissionsSucceeded = 0;
+  var imgHexagonFill = '';
+
+  // Récupération de l'état de complétion des activités
+  $http({
+    method: 'GET',
+    url: 'http://hexagon-api-dev.comem.ch/users/'+ actualUserId,
+  }).then(function(res) {
+
+    // Si la mission développement est remplie
+    if(res.data.codingDone) {
+      nbMissionsSucceeded++;
+    }
+
+    // Si la mission marketing est remplie
+    if(res.data.marketingComDone) {
+      nbMissionsSucceeded++;
+    }
+
+    // Si la mission business/management est remplie
+    if(res.data.businessManagementDone) {
+      nbMissionsSucceeded++;
+    }
+
+    // Si la mission multimedia est remplie
+    if(res.data.multimediaDone) {
+      nbMissionsSucceeded++;
+    }
+
+    // Affichage des images en fonction du nombre de missions complétées
+    switch (nbMissionsSucceeded) {
+      case 1:
+        imgHexagonFill = 'jauge_1.png';
+        break;
+      case 2:
+        imgHexagonFill = 'jauge_2.png';
+        break;
+      case 3:
+        imgHexagonFill = 'jauge_3.png';
+        break;
+      case 4:
+        imgHexagonFill = 'jauge_4.png';
+        $scope.hrefHexagonImg = '/#!/arbitrium';
+        break;
+      default:
+    }
+
+    // Changement du nom de l'image dans le scope
+    $scope.imageHexagon = imgHexagonFill;
+
   });
+
+});
